30e4f6ad0af88e4992164cb0abffe6b5a2ddad9c
Goktug Mac <goktugsenkal1881@gmail.com>
2024-09-28 03:13:08 +0300

    Add endpoints structure

--- Api/Controllers/BusinessesController.cs
+++ Api/Controllers/BusinessesController.cs
@@ -1,6 +1,40 @@
+using Api.Dtos;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Controllers;
 
-public class BusinessesController : BaseApiController
+[ApiController]
+[Route("api/business")]
+public class BusinessesController : ControllerBase
 {
-    
+    [HttpGet]
+    public Task<ActionResult<IReadOnlyList<Business>>> GetBusinesses()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id:int}")]
+    public Task<ActionResult<Business>> GetBusinessById()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public ActionResult<Business> CreateBusiness(CreateBusinessDto business)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<Business> UpdateBusiness(UpdateBusinessDto business, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult DeleteBusiness(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
