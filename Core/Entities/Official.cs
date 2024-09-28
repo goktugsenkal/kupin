78b4296ddd911fea4c9644d32a90c6e02c0537f9
Goktug Mac <goktugsenkal1881@gmail.com>
2024-09-28 03:14:46 +0300

    Add FK to ContactInfo

--- Core/Entities/Official.cs
+++ Core/Entities/Official.cs
@@ -2,7 +2,10 @@ namespace Core.Entities;
 
 public class Official : BaseEntity
 {
-    public required string Name { get; set; }
-    public required string MailAddress { get; set; }
-    public required string PhoneNumber { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string MailAddress { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    // foreign key
+    public int ContactInfoId { get; set; }
 }
