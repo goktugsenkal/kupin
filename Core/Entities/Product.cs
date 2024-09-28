c818feb7a00029e02b5fa8ed02a24e7e40fe9d97
Goktug Mac <goktugsenkal1881@gmail.com>
2024-09-28 03:15:05 +0300

    Remove navigation property

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -2,13 +2,11 @@ namespace Core.Entities;
 
 public class Product : BaseEntity
 {
-    public required string Name { get; set; }
-    public decimal Price { get; set; }
-    public string? ImageUrl { get; set; }
-    public string? Description { get; set; }
-    // Stock
-    public int StockQuantity { get; set; }
-    // Relationship with business
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public int QuantityInStock { get; set; }
+    
+    // foreign key
     public int BusinessId { get; set; }
-    public required Business Business { get; set; }
 }
