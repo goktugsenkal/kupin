e8c01bfc631b63e31c1b1c750334ed8a55d5fd97
Goktug Mac <goktugsenkal1881@gmail.com>
2024-09-28 03:12:55 +0300

    Add navigation properties

--- Core/Entities/Business.cs
+++ Core/Entities/Business.cs
@@ -2,21 +2,16 @@ namespace Core.Entities;
 
 public class Business : BaseEntity
 {
-    public required string Name { get; set; }
-    public string? ImageUrl { get; set; }
-    public required string BusinessType { get; set; }
-    public Address? Address { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public string BusinessType { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+
+    // one(business) to many
+    public List<Address> Addresses { get; set; } = new List<Address>();
+    public List<Product> Products { get; set; } = new List<Product>();
+
+    // reference navigation to dependent (ContactInfo) (1 : 1)
     public ContactInfo? ContactInfo { get; set; }
-    public List<Product> Products { get; set; } = [];
-    public bool ActiveStatus { get; set; }
-
-    public void ActivateBusiness()
-    {
-        ActiveStatus = true;
-    }
-
-    public void DeactivateBusiness()
-    {
-        ActiveStatus = false;
-    }
 }
