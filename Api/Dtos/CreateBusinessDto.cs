327659511bc69d48b08fd371b5ddb6b2fd78afbc
goktug <goktugsenkal1881@gmail.com>
2024-09-30 14:02:01 +0300

    update CreateBusinessDto

--- Api/Dtos/CreateBusinessDto.cs
+++ Api/Dtos/CreateBusinessDto.cs
@@ -5,7 +5,9 @@ namespace Api.Dtos;
 public class CreateBusinessDto
 {
     [Length(2, 15)]
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
     public string? ImageUrl { get; set; }
+    [AllowedValues("Restaurant", "Cafe", "Gym", ErrorMessage = "Business Type can be one of the following values: Restaurant, Cafe, Gym")]
+    public required string BusinessType { get; set; }
 }
