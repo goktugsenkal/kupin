eced87152100920c263694760128eca23f0d880f
goktug <goktugsenkal1881@gmail.com>
2024-09-30 15:17:38 +0300

    add props to UpdateBusinessDto

--- Api/Dtos/UpdateBusinessDto.cs
+++ Api/Dtos/UpdateBusinessDto.cs
@@ -1,6 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.Dtos;
 
 public class UpdateBusinessDto
 {
-    
+    public int Id { get; set; }
+    [Length(2, 15)]
+    public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    [AllowedValues("Restaurant", "Cafe", "Gym", ErrorMessage = "Business Type can be one of the following values: Restaurant, Cafe, Gym")]
+    public required string BusinessType { get; set; }
 }
