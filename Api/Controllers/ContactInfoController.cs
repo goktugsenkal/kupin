702a6db38e6da38c9f2684576ee6adc921a74a35
goktug <goktugsenkal1881@gmail.com>
2024-10-01 14:45:37 +0300

    Add ContactInfoControllerand  get all & get one endpoints

---
+++ Api/Controllers/ContactInfoController.cs
@@ -0,0 +1,29 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/business/{businessId:int}/contactInfo")]
+public class ContactInfoController
+    (IContactInfoRepository contactInfoRepository) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<ContactInfo>> GetContactInfo(int businessId)
+    {
+        var contactInfo = await contactInfoRepository
+            .GetAllContactInfoAsync(businessId);
+        
+        return Ok(contactInfo);
+    }
+
+    [HttpGet("{contactInfoId:int}")]
+    public async Task<ActionResult<ContactInfo>> GetContactInfo(int businessId, int contactInfoId)
+    {
+        var contactInfo = await contactInfoRepository
+            .GetContactInfoByIdAsync(businessId, contactInfoId);
+        
+        return Ok(contactInfo);
+    }
+}
