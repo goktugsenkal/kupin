ce649d883d99fbfd6befe0688290c76380cf97b6
Goktug Mac <goktugsenkal1881@gmail.com>
2024-10-05 03:51:44 +0300

    Add CreateOfficialDto[] -> Official[] mapper extension method

--- Api/Dtos/MappingExtensions.cs
+++ Api/Dtos/MappingExtensions.cs
@@ -1,6 +1,24 @@
+using Core.Entities;
+
 namespace Api.Dtos;
 
-public class MappingExtensions
+public static class MappingExtensions
 {
-    
+    public static List<Official> ToListOfOfficials(this List<CreateOfficialDto> dtos)
+    {
+        var officials = new List<Official>();
+        
+        foreach (var dto in dtos)
+        {
+            var official = new Official
+            {
+                Name = dto.Name,
+                MailAddress = dto.MailAddress,
+                PhoneNumber = dto.PhoneNumber
+            };
+            officials.Add(official);
+        }
+
+        return officials;
+    }
 }
