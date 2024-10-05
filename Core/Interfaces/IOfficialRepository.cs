4a2fbdc0fd64c72f0759c2cf247ccd43f4449eb1
Goktug Mac <goktugsenkal1881@gmail.com>
2024-10-05 03:50:45 +0300

    Add Official repo & interface

--- Core/Interfaces/IOfficialRepository.cs
+++ Core/Interfaces/IOfficialRepository.cs
@@ -1,6 +1,14 @@
+using Core.Entities;
+
 namespace Core.Interfaces;
 
 public interface IOfficialRepository
 {
-    
+    Task<IReadOnlyList<Official>> GetAllOfficialsAsync
+        (int contactInfoId);
+    Task<Official> GetOfficialByIdAsync
+        (int businessId, int contactInfoId, int officialId);
+    void CreateOfficial(Official official);
+    void UpdateOfficial(Official official);
+    void DeleteOfficial(Official official);
 }
--- Infrastructure/Data/OfficialRepository.cs
+++ Infrastructure/Data/OfficialRepository.cs
@@ -1,6 +1,36 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.Data;
 
-public class OfficialRepository
+public class OfficialRepository(DataContext context) : IOfficialRepository
 {
-    
+    public async Task<IReadOnlyList<Official>> GetAllOfficialsAsync(int contactInfoId)
+    {
+        return await context.Official
+            .Where(o => o.Id == contactInfoId)
+            .ToListAsync();
+    }
+
+    public Task<Official> GetOfficialByIdAsync
+        (int businessId, int contactInfoId, int officialId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateOfficial(Official official)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateOfficial(Official official)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteOfficial(Official official)
+    {
+        throw new NotImplementedException();
+    }
 }
