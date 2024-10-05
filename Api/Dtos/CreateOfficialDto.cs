891a6b0bf65eac93ad434e5ba647326e94393dbc
Goktug Mac <goktugsenkal1881@gmail.com>
2024-10-05 03:50:05 +0300

    Add CreateOfficialDto

--- Api/Dtos/CreateOfficialDto.cs
+++ Api/Dtos/CreateOfficialDto.cs
@@ -2,5 +2,7 @@ namespace Api.Dtos;
 
 public class CreateOfficialDto
 {
-    
+    public string Name { get; set; } = string.Empty;
+    public string MailAddress { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
 }
