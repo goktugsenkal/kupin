3eb99db4bca4cb87c4605947edcbfad2c51067c8
Goktug Mac <goktugsenkal1881@gmail.com>
2024-10-10 00:06:36 +0300

    fix typo

--- Core/Interfaces/IOfficialRepository.cs
+++ Core/Interfaces/IOfficialRepository.cs
@@ -5,9 +5,9 @@ namespace Core.Interfaces;
 public interface IOfficialRepository
 {
     Task<IReadOnlyList<Official>> GetAllOfficialsAsync
-        (int contactInfoId);
-    Task<Official> GetOfficialByIdAsync
-        (int businessId, int contactInfoId, int officialId);
+        (int businessId);
+    Task<Official?> GetOfficialByIdAsync
+        (int businessId, int officialId);
     void CreateOfficial(Official official);
     void UpdateOfficial(Official official);
     void DeleteOfficial(Official official);
